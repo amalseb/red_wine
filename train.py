678a8a0e7fadad49567ca980246bc617c662da79
Amal Sebastian <amalseban@localhost.localdomain>
2020-10-02 11:37:47 +0530

    first commit

---
+++ train.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from xgboost import XGBRegressor
+
+df = pd.read_csv("winequality-red.csv")
+
+y = df.quality
+X = df.drop(columns=['quality'])
+
+X_train_valid,X_test,y_train_valid,y_test = train_test_split(X, y, train_size=0.9, test_size=0.1,random_state=0)
+
+X_train,X_valid,y_train,y_valid = train_test_split(X_train_valid, y_train_valid, train_size=0.75, test_size=0.25,random_state=0)
+
+categorical_cols = [cname for cname in X_train.columns if
+                    X_train[cname].dtype == "object"]
+
+numerical_cols = [cname for cname in X_train.columns if 
+                X_train[cname].dtype in ['int64', 'float64']]
+
+numerical_transformer = SimpleImputer(strategy='constant')
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot',OneHotEncoder(handle_unknown='ignore', sparse=False))
+])
+
+preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_cols),
+            ('cat',categorical_transformer,categorical_cols)
+        ]
+)
+
+
+model = XGBRegressor(n_estimators=10000,random_state=0,learning_rate=0.001,max_depth=8)
+
+pipeline = Pipeline(steps=[('preprocessor',preprocessor),
+                          ])
+
+
+X_train = pipeline.fit_transform(X_train)
+X_valid = pipeline.transform(X_valid)
+X_test = pipeline.transform(X_test)
+
+model.fit(X_train,y_train, eval_set=[(X_valid,y_valid)], early_stopping_rounds=15)
+
+
+# Report training set score
+train_score = model.score(X_train, y_train) * 100
+# Report test set score
+test_score = model.score(X_test, y_test) * 100
+
+# Write scores to a file
+with open("metrics.txt", 'w') as outfile:
+        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+
+
+##########################################
+##### PLOT FEATURE IMPORTANCE ############
+##########################################
+# Calculate feature importance in random forest
+importances = model.feature_importances_
+labels = df.columns
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+feature_df = feature_df.sort_values(by='importance', ascending=False,)
+
+# image formatting
+axis_fs = 18 #fontsize
+title_fs = 22 #fontsize
+sns.set(style="whitegrid")
+
+ax = sns.barplot(x="importance", y="feature", data=feature_df)
+ax.set_xlabel('Importance',fontsize = axis_fs) 
+ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
+ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
+
+plt.tight_layout()
+plt.savefig("feature_importance.png",dpi=120) 
+plt.close()
+
+
+##########################################
+############ PLOT RESIDUALS  #############
+##########################################
+
+y_pred = model.predict(X_test) + np.random.normal(0,0.25,len(y_test))
+y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
+res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+
+ax = sns.scatterplot(x="true", y="pred",data=res_df)
+ax.set_aspect('equal')
+ax.set_xlabel('True wine quality',fontsize = axis_fs) 
+ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+ax.set_title('Residuals', fontsize = title_fs)
+
+# Make it pretty- square aspect ratio
+ax.plot([1, 10], [1, 10], 'black', linewidth=1)
+plt.ylim((2.5,8.5))
+plt.xlim((2.5,8.5))
+
+plt.tight_layout()
+plt.savefig("residuals.png",dpi=120) 
