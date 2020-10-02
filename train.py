b9aa825b971c210d43f84c3a9094c0a3913870b7
Amal Sebastian <amalvallonthayil@gmail.com>
2020-10-03 00:35:44 +0530

    Update train.py

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ preprocessor = ColumnTransformer(
 )
 
 
-model = XGBRegressor(n_estimators=10000,random_state=0,learning_rate=0.001,max_depth=8)
+model = XGBRegressor(n_estimators=10000,random_state=0,learning_rate=0.001,max_depth=7)
 
 pipeline = Pipeline(steps=[('preprocessor',preprocessor),
                           ])
